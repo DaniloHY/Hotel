22c6aa0ec6720f5141ee8f0ba6d4cff706d0e979
DaniloHY <nilo.11.dhy@gmail.com>
2022-09-29 22:12:47 -0300

    Hotel

---
+++ app/src/main/java/br/com/etecia/hotel/Hotel4Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.hotel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Hotel4Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.hotel4_layout);
+    }
+}
